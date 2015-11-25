494ecec16ccabb13da88a6b0c1632a96a95d7235
Thanh Tran <mr.thanh.tct@gmail.com>
2015-11-25 20:57:10 +0100

    Initial Commit

---
+++ prob2.4/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  prob2.4
+//
+//  Created by Thanh C. Tran on 25/11/15.
+//  Copyright © 2015 Thanh C. Tran. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
